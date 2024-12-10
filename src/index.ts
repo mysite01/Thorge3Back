ced865f3dc668280e91d3eca17077ce31a8f3a3c
Tassnim <s90301@bht-berlin.de>
2024-12-10 18:23:28 +0100

    erster Commit

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import cors from 'cors';
 
 
 const corsOptions = {
-  origin: 'https://thorge3-front.vercel.app', // Ersetze dies mit deiner tatsächlichen Frontend-Domain
+  origin: 'https://geo-pick-points.vercel.app', // Ersetze dies mit deiner tatsächlichen Frontend-Domain
   methods: 'GET,POST,PUT,DELETE',
   allowedHeaders: 'Content-Type, Authorization', // Erlaubte Header
   credentials: true, // Falls du Cookies oder Authentifizierung benötigst
